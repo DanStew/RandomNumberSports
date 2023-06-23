d5590ac01ef2076a8651e59435510ec3e331292a
DanStew <90531180+DanStew@users.noreply.github.com>
2023-06-23 16:33:12 +0100

    Small Screen Error Implemented

--- JavascriptFiles/baseballJS.js
+++ JavascriptFiles/baseballJS.js
@@ -1,6 +1,7 @@
 //Initialising key variables
 let screenMode = 0;
 let opposingTeam = "The Computer";
+let passScreenSize = true;
 
 //Initialising the timer increment in the system
 let counter=0
@@ -15,41 +16,64 @@ let inp1 ;
 let button1;
 
 function setup() {
-    createCanvas(windowWidth*0.97, windowHeight*0.80)
+    if(windowWidth < 1240 || windowHeight < 460){
+      createCanvas(windowWidth*0.97,windowHeight*0.8)
+      background(255)
+      passScreenSize=false
+    }
+    else{
+      createCanvas(1240, 460)
 
-    //Setting up the inputs and buttons used in the system
-    inputSetup()
-    buttonSetup()
-
-    //Function to set up the timer for the transition screen
-    setInterval(timeIt,1000)
+      //Setting up the inputs and buttons used in the system
+      inputSetup()
+      buttonSetup()
+  
+      //Function to set up the timer for the transition screen
+      setInterval(timeIt,1000)
+    }
   }
   
   function draw() {
+    if (passScreenSize == true){
+      //Setting up the home screen of the game
+      if (screenMode == 0 && screen0Displayed==false){
+        MainScreenSetup()
+      }
 
-    //Setting up the home screen of the game
-    if (screenMode == 0 && screen0Displayed==false){
-      MainScreenSetup()
-    }
+      //Setting up the transition screen of the website
+      if (screenMode == 1 && screen1SDisplayed==false){
+        screen1Setup()
+      }
+      else if (screenMode == 1){
+        if (counter >= 10){
+          screenMode = 2
+        }
+      }
 
-    //Setting up the transition screen of the website
-    if (screenMode == 1 && screen1SDisplayed==false){
-      screen1Setup()
-    }
-    else if (screenMode == 1){
-      if (counter >= 10){
-        screenMode = 2
+      //Setting up the play screen of the website (Batting)
+      if (screenMode == 2 && screen2SDisplayed==false){
+        screen2Setup()
       }
     }
-
-    //Setting up the play screen of the website (Batting)
-    if (screenMode == 2 && screen2SDisplayed==false){
-      screen2Setup()
-    }
+    else{
+      smallScreenSetup()
+    } 
   }
 
 
 //Setup Functions
+
+  //Function to setup the screen for when the users screen is too small
+  function smallScreenSetup(){
+    textSize(50)
+    textStyle(BOLD)
+    text("Screen Size Issue", windowWidth*0.15, windowHeight*0.1)
+    textStyle(NORMAL)
+    textSize(40)
+    text("Screen is too small", windowWidth*0.2, windowHeight*0.3)
+    text("to display game", windowWidth*0.26, windowHeight*0.40)
+    text("Please resize screen and refresh", 10, windowHeight*0.60)
+  }
 
   //Function to set up the main text and background of the home screen
   function MainScreenSetup(){
@@ -57,12 +81,12 @@ function setup() {
     background(120,240,130)
 
     //The text of the main screen
-    textSize(50)
+    textSize(80)
     textStyle(BOLD)
-    text("BASEBALL", windowWidth*0.3, windowHeight*0.2)
-    textSize(32)
+    text("BASEBALL", 394, 108)
+    textSize(56)
     textStyle(NORMAL)
-    text("Opposing Team : ", windowWidth*0.1, windowHeight*0.4)
+    text("Opposing Team : ", 124, 240)
 
     //Showing the inputs and buttons needed for the page
     inp1.show()
@@ -75,6 +99,7 @@ function setup() {
   //Function to setup the code for the transition page (Between Innings)
   function screen1Setup(){
     counter=0
+    console.log(opposingTeam)
     screen1SDisplayed = true
   }
 
@@ -87,16 +112,16 @@ function setup() {
     button1.hide()
 
     //Code to split the screen into segments
-    line(windowWidth*0.25,0,windowWidth*0.25,windowHeight*0.8)
-    line(windowWidth*0.75,0,windowWidth*0.75,windowHeight*0.8)
+    line(310,0,310,460)
+    line(930,0,930,460)
   }
 
   function inputSetup(){
 
     //Creating the input box for the Opposing Team (Main Screen)
     inp1 = createInput('')
-    inp1.position(windowWidth*0.64, windowHeight*0.6)
-    inp1.size(160)
+    inp1.position(760, 336)
+    inp1.size(300,60)
     inp1.input(ChangeOpposingTeam)
     inp1.hide()
   
@@ -106,7 +131,8 @@ function setup() {
   
     //Creating the button to start the game (Main Screen)
     button1 = createButton("Start Game")
-    button1.position(windowWidth*0.4,windowHeight*0.85)
+    button1.size(180,60)
+    button1.position(520,440)
     button1.mousePressed(StartGame)
   
   }
